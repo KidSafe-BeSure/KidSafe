67194219d764d72e95196eb445d4e377d3edc2c1
KidSafe-BeSure <KidSafe.BeSure@gmail.com>
2016-04-16 11:14:17 +0300

    Added gitignore fix and test comment.

--- app/src/main/java/com/example/maya/kidsafe/MainActivity.java
+++ app/src/main/java/com/example/maya/kidsafe/MainActivity.java
@@ -19,7 +19,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
-
+    /// Some test comment for github
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private static final String TAG = "MainActivity";
 
